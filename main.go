d0d78881c68f78d82c5c6f5dcde3b72da81134d2
umuttopalak <umuttopalak@hotmail.com>
2023-08-28 16:28:26 +0300

    düzeltme

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Item struct {
 	Name     string
@@ -14,7 +16,7 @@ type Describable interface {
 
 func (i Item) Description() string {
 	if i.Discount != 0 {
-		return fmt.Sprintf("%s - %2.f TL (%2.f%% İndirimli %2.f TL)", i.Name, i.Price, i.Discount, i.Price-i.Discount)
+		return fmt.Sprintf("%s - %2.f TL (%2.f%% İndirimli %2.f TL)", i.Name, i.Price, i.Discount, calculatePrice(i))
 	}
 
 	return fmt.Sprintf("%s - %2.f TL", i.Name, i.Price)
@@ -23,10 +25,9 @@ func (i Item) Description() string {
 func calculatePrice(item Item) float64 {
 
 	if item.Discount != 0 {
-		fmt.Println(item.Description())
-		return item.Price - item.Discount
+		return (item.Price - ((item.Price * item.Discount) / 100))
 	}
-	fmt.Println(item.Description())
+
 	return item.Price
 }
 
@@ -42,11 +43,15 @@ func totalPrice(items []Item) float64 {
 }
 
 func main() {
-	item1 := Item{Name: "Elma", Price: 10, Discount: 2}
-	item2 := Item{Name: "Armut", Price: 20, Discount: 3}
+	item1 := Item{Name: "Elma", Price: 10, Discount: 20}
+	item2 := Item{Name: "Armut", Price: 20, Discount: 30}
 	item3 := Item{Name: "Şeftali", Price: 30, Discount: 0}
 
 	items := []Item{item1, item2, item3}
+
+	for _, i := range items {
+		fmt.Println(i.Description())
+	}
 
 	fmt.Println("Toplam Fiyat: ", totalPrice(items))
 }
