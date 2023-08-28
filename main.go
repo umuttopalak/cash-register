a72242acc64b7071f4008fc6ad2f5c81bb020699
umuttopalak <umuttopalak@hotmail.com>
2023-08-28 16:20:31 +0300

    fonksiyonlar eklendi

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import "fmt"
 
+type Item struct {
+	Name     string
+	Price    float64
+	Discount float64
+}
+
+type Describable interface {
+	Description() string
+}
+
+func (i Item) Description() string {
+	if i.Discount != 0 {
+		return fmt.Sprintf("%s - %2.f TL (%2.f%% İndirimli %2.f TL)", i.Name, i.Price, i.Discount, i.Price-i.Discount)
+	}
+
+	return fmt.Sprintf("%s - %2.f TL", i.Name, i.Price)
+}
+
+func calculatePrice(item Item) float64 {
+
+	if item.Discount != 0 {
+		fmt.Println(item.Description())
+		return item.Price - item.Discount
+	}
+	fmt.Println(item.Description())
+	return item.Price
+}
+
+func totalPrice(items []Item) float64 {
+
+	var total float64
+
+	for _, i := range items {
+		total += calculatePrice(i)
+	}
+
+	return total
+}
+
+func main() {
+	item1 := Item{Name: "Elma", Price: 10, Discount: 2}
+	item2 := Item{Name: "Armut", Price: 20, Discount: 3}
+	item3 := Item{Name: "Şeftali", Price: 30, Discount: 0}
+
+	items := []Item{item1, item2, item3}
+
+	fmt.Println("Toplam Fiyat: ", totalPrice(items))
 }
